97661033af5dc3c179035e7da9840b47638142a5
Jacob Minshall <minshall.jacob@yahoo.com>
2013-03-18 23:40:43 -0700

    added in old assignments
    each assignment probably had it's own git repo inside of it, so I wonder
    how this will work with being wrapped in a new git repo, this should be
    some good git testing.

---
+++ ass5/stats.c
@@ -0,0 +1,16 @@
+// stats.c
+// jacob minshall
+//
+// input variable length array with integer values and compute
+// mean median and mode
+//
+
+#include <iostream>
+using namespace std;
+
+bool read_input(int * arr, int * size);
+double median(int * arr, int size);
+double mode(int * arr, int size);
+double average(int * arr, int size);
+
+
---
+++ memry.c
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  char letter = 'a';
+  int number;
+  double floater;
+
+  cout << "Taking time " << endl;
+  cout << &letter << endl;
+  cout << &number << endl;
+  cout << &floater << endl;
+
+  return 0;
+}
